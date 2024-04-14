60fe7809e39726bef732bfbc133d341b48c5c718
AyushRay06 <rayayush47@gmail.com>
2024-04-14 18:03:45 +0530

    Auth-comp-Added

--- blog-main/frontend/src/App.tsx
+++ blog-main/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import { Signup } from "./pages/Signup"
 import { Signin } from "./pages/Signin"
 import { Blog } from "./pages/Blog"
--- blog-main/frontend/src/components/Auth.tsx
+++ blog-main/frontend/src/components/Auth.tsx
@@ -1,18 +1,93 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { SignupInput } from "@100xdevs/medium-common"
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const [postInputs, setPostInputs] = useState<SignupInput>({
+    name: "",
+    username: "",
+    password: "",
+  })
+
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
         <div>
-          <div className="text-3xl font-extrabold">Create an account</div>
-          <div className="text-slate-600 font-light mt-1">
-            Already have an account?
-            <Link className="underline" to={"/signin"}>
-              Login
-            </Link>
+          <div>
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-600 font-light mt-1">
+              Already have an account?
+              <Link className="underline pl-1" to={"/signin"}>
+                Login
+              </Link>
+            </div>
+          </div>
+
+          <div>
+            <LabelledInput
+              label="Name"
+              placeholder="Ayush Ray"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  name: e.target.value,
+                })
+              }}
+            />
+            <LabelledInput
+              label="Username"
+              placeholder="ayushrau@gmail.com"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  username: e.target.value,
+                })
+              }}
+            />
+            <LabelledInput
+              label="Password"
+              placeholder="*********"
+              type={"password"}
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  password: e.target.value,
+                })
+              }}
+            />
           </div>
         </div>
+      </div>
+    </div>
+  )
+}
+
+interface LabelledInputTypes {
+  label: string
+  placeholder: string
+  onChange: (e: any) => void
+  type?: string
+}
+
+export const LabelledInput = ({
+  label,
+  placeholder,
+  onChange,
+}: LabelledInputTypes) => {
+  return (
+    <div>
+      <div>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          {label}
+        </label>
+        <input
+          onChange={onChange}
+          type="text"
+          id="first_name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder={placeholder}
+          required
+        />
       </div>
     </div>
   )
