cd5087bceaf471458338cdaf028222c98c8d8803
AyushRay06 <rayayush47@gmail.com>
2024-04-14 15:25:35 +0530

    Quote-comp-added

---
+++ blog-main/frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className="text-3xl font-bold">
+            "The coutomer survice i recived was exceptional.The Support team
+            went above and beyond to adress my concerns."
+          </div>
+          <div className="font-semibold max-w-md text-left text-xl pt-4">
+            Jules Standford
+          </div>
+          <div className="text-slate-600 md-w-md text-left pt-1">
+            CEO, Acrene Inc
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
