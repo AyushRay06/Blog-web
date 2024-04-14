22ce49af1892aab7ef8e8cee569011ccdea70c3a
AyushRay06 <rayayush47@gmail.com>
2024-04-14 19:08:49 +0530

    Dynamic-btn-added

--- blog-main/frontend/src/components/Auth.tsx
+++ blog-main/frontend/src/components/Auth.tsx
@@ -19,9 +19,14 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
           <div className="px-20">
             <div className="text-3xl font-extrabold">Create an account</div>
             <div className="text-slate-600 font-light mt-1">
-              Already have an account?
-              <Link className="underline pl-1" to={"/signin"}>
-                Login
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <Link
+                className="underline pl-1"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Signup" : "Signin"}
               </Link>
             </div>
           </div>
@@ -59,9 +64,13 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
               }}
             />
           </div>
-          <Button />
-          <GoogleButton />
-          <GithubButton />
+
+          <button
+            type="button"
+            className="w-full text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-40 py-3 text-center me-2 mb-2 mt-10"
+          >
+            {type === "signin" ? "SIGN IN" : "SIGN UP"}
+          </button>
         </div>
       </div>
     </div>
--- blog-main/frontend/src/components/Button.tsx
+++ blog-main/frontend/src/components/Button.tsx
@@ -1,10 +1,10 @@
-export const Button = () => {
+export const Button = ({ type }: { type: "signup" | "signin" }) => {
   return (
     <button
       type="button"
       className="w-full text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-40 py-3 text-center me-2 mb-2 mt-10"
     >
-      SIGN UP
+      {type === "signin" ? "SIGN IN" : "SIGN UP"}
     </button>
   )
 }
